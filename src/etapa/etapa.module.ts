b129f72d992f4f6e402e416a6103562bced7e231
carlosalcedo09 <carlosalcedojavier@gmail.com>
2023-05-14 22:26:46 -0500

    Tabla Etapa

---
+++ src/etapa/etapa.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { EtapaController } from './etapa.controller';
+import { EtapaService } from './etapa.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Etapa } from './etapa.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Etapa])],
+  controllers: [EtapaController],
+  providers: [EtapaService]
+})
+export class EtapaModule {}
